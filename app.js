16810f3117015b68bcce7088d3586b6d1d0a6df4
ltrzalez <ltrzalez@gmail.com>
2021-03-03 20:05:11 -0300

    updates

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
-// modulos requeridos. Ejecutar en consola " npm install express mysql " para hacer funcionar el proyecto
+// modulos requeridos. Ejecutar en consola " npm install express mysql util" para hacer funcionar el proyecto
 const express = require('express');
 const mysql = require('mysql');
-// instaciamos express y definimos el puerto donde va estar recibiendo peticiones  
+const util = require('util');
+// instaciamos express definimos el puerto donde va estar recibiendo peticiones y le permitimos a la pasear json
 const app = express();
 const puerto = 8080;
+app.use(express.json());
 // completar con las credenciales de la base de datos
 const conexion = mysql.createConnection({
-    host: '',
+    host: 'localhost',
     user: '',
     password: '',
     database: ''
 })
-// conexion.connect()  valga la redundancia establecemos la conexion a la base de datos esta comentado sino crashea al ejecutarse
+/*  valga la redundancia establecemos la conexion a la base de datos. Esta comentado sino crashea al ejecutarse
+conexion.connect((error)=>{
+    if(error) {
+        throw error
+    };
+    console.log('conectado a la base de datos')
+}) 
+*/
+
+const qy = util.promisify(conexion.query).bind(conexion); // permite uso de async await
 
 
 
+//deberia resoponder con el index.htlm
 app.get('/', (req, res) => {
     res.send('trabajo practico')
 })
 
-app.get('/categoria', (req, res) => {
+// 
+
+const selectAll =  'SELECT * FROM '
+
+app.get('/categoria', async (req, res) => {
+    try {
+        const getCategorias = await qy( `${selectAll} categoria`);
+        res.send(getCategorias)
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(413).send({"Hubo un error": e.message})        
+    }    
+
+
     res.send('obteniendo categorias')
 })
 
@@ -31,7 +57,12 @@ app.get('/libro', (req, res) => {
     res.send('obteniendo libro')
 })
 
+
+
+
+
+
 // ponemos la applicacion a "escuchar" las peticiones
 app.listen(puerto, () =>{
-    console.log('server on')
+    console.log('server on' , puerto)
 })
